6873e496ff8388381d2ca9be2ee274c07a6d853a
KIMHEEJIN <hee031812@gmail.com>
2024-01-25 11:24:31 +0900

    first commit

---
+++ server/config/dev.js
@@ -0,0 +1,7 @@
+module.exports = {
+    mongoURI: "mongodb+srv://PETPAR:petpar1234@cluster0.ibifabh.mongodb.net/?retryWrites=true&w=majority",
+    access_key: "9MEVnFsE4gaM3KbbrG97",
+    secret_key: "txOKTZtb7hdClNWNZLC71wmUggseIlqWsEwuhrGf",
+    NAVER_API_KEY: "i3BGqPLM4tsx9KPWMyNQGbUzRKIPhDFIzqAWfJou",
+    NAVER_API_KEY_ID: "ztpqx02uq3",
+}
